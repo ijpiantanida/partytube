d3c957c60afa64d6a43696a8c2e6cbc2d21177d8
Ignacio Piantanida <ijpiantanida@gmail.com>
2013-01-05 20:58:52 -0300

    Fixed typo

--- client/templates/player_action_control.js
+++ client/templates/player_action_control.js
@@ -1,7 +1,7 @@
 Template.player_control.events({
     'click input.next':function(){
         TemplatesHelpers.selected_list().with_next_song_do(function(next_song){
-            Partytube.player_control.paySong(next_song);
+            Partytube.player_control.playSong(next_song);
         });
     },
     'click input.play':function(){
